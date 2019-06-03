0e9c513c9ccbb6cab51f4b7c56e17c83d74741d1
Paul <thatgreenguy@techie.com>
2019-06-03 22:56:18 +0100

    Introduce swagger

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
+import { getPlacesByName } from "./SearchController";
+import { checkSearchParams } from "../../middleware/checks";
 
 export default [
   {
-    path: "/",
+    path: "/api/v1/search",
     method: "get",
-    handler: async (req: Request, res: Response) => {
-      res.send("Hello world!");
-    }
+    handler: [
+      checkSearchParams, // <-- this line
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q);
+        res.status(200).send(result);
+      }
+    ]
   }
 ];
